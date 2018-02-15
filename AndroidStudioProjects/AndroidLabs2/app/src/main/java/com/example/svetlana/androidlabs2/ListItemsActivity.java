4694676a2fd1a63e89cc3f43401312576854bd52
Svetlana <netc0004@algonquinlive.com>
2018-02-14 19:16:20 -0500

    Added 3 activities for Lab 2

---
+++ AndroidStudioProjects/AndroidLabs2/app/src/main/java/com/example/svetlana/androidlabs2/ListItemsActivity.java
@@ -0,0 +1,14 @@
+package com.example.svetlana.androidlabs2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListItemsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+
+    }
+}
